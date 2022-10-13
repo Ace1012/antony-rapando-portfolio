6bc5ad4157a444c101ed48144b245d2b4e4f5d5b
Ace1012 <antonyrapando46@gmail.com>
2022-10-13 15:24:43 +0300

    -Adding base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/antony-rapando-portfolio/',
+  base:'/antony-rapando-portfolio/',
+  plugins: [react()]
 })
